b5744e56fc16805eef7a6f8f4a5e16f4767133be
Bernard Potocki <bernard.potocki@imanel.org>
2020-03-15 19:01:50 +0100

    Fix lint warning

--- src/components/Field/index.js
+++ src/components/Field/index.js
@@ -26,7 +26,7 @@ const Field = (props) => {
         "name": <input type="text" value={name} onChange={(evt) => changeName(dispatch, props.id, evt.target.value)} />,
         "type":
         <select value={type} onChange={(evt) => changeType(dispatch, props.id, evt.target.value)}>
-          {supportedTypes.map(st => <option value={st}>{st}</option>)}
+          {supportedTypes.map(st => <option key={st} value={st}>{st}</option>)}
         </select>
         {!props.parentId ? null :
           <React.Fragment>, "nullable": <input type="checkbox" checked={nullable} onChange={(evt) => changeNullable(dispatch, props.id)} /></React.Fragment>
