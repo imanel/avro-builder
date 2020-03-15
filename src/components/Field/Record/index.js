f5f07437f77635974bdd8ffdd179ac807d6c2581
Bernard Potocki <bernard.potocki@imanel.org>
2020-03-15 18:59:16 +0100

    Change type selection to select box

--- src/components/Field/Record/index.js
+++ src/components/Field/Record/index.js
@@ -4,13 +4,13 @@ import FieldList from 'components/FieldList'
 import { useStore } from 'store'
 import { changeDefaultValue } from 'store/actions'
 
-const RecordType = ({ id, parentId, default: defaultValue }) => {
+const RecordType = ({ id, parentId, nullable, default: defaultValue }) => {
   const [, dispatch] = useStore()
   const DefaultField = <React.Fragment>, "default": <input type="text" value={defaultValue} onChange={(evt) => changeDefaultValue(dispatch, id, evt.target.value)} /></React.Fragment>
 
   return (
     <React.Fragment>
-      {parentId ? DefaultField : null}
+      {parentId && !nullable ? DefaultField : null}
       , "fields": [
         <FieldList parentId={id} />
       ]
--- src/components/Field/index.js
+++ src/components/Field/index.js
@@ -4,13 +4,14 @@ import RecordField from './Record'
 import { useStore } from 'store'
 import { changeName, changeNullable, changeType, removeField } from 'store/actions'
 
+const supportedTypes = ['boolean', 'int', 'long', 'float', 'double', 'bytes', 'string', 'record', 'enum', 'array', 'map']
+
 const Field = (props) => {
   const [, dispatch] = useStore()
 
   const { name, nullable, type } = props
 
   let TypeComponent = null
-
   switch (type) {
     case 'record': TypeComponent = RecordField; break
     default: break
@@ -23,7 +24,10 @@ const Field = (props) => {
       }
       &#123;
         "name": <input type="text" value={name} onChange={(evt) => changeName(dispatch, props.id, evt.target.value)} />,
-        "type": <input type="text" value={type} onChange={(evt) => changeType(dispatch, props.id, evt.target.value)} />
+        "type":
+        <select value={type} onChange={(evt) => changeType(dispatch, props.id, evt.target.value)}>
+          {supportedTypes.map(st => <option value={st}>{st}</option>)}
+        </select>
         {!props.parentId ? null :
           <React.Fragment>, "nullable": <input type="checkbox" checked={nullable} onChange={(evt) => changeNullable(dispatch, props.id)} /></React.Fragment>
         }
