dc67d04101c4661cf5631fe21d828f6191d43377
Bernard Potocki <bernard.potocki@imanel.org>
2020-03-15 23:17:56 +0100

    Updates

---
+++ precache-manifest.b29ed32a7fd3e8e33bd90a418e9ca21e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd1ac802efb06edeadcb610b6733d8b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3dd52b0f1562c0de2e5c",
+    "url": "/static/css/main.dbab1ad8.chunk.css"
+  },
+  {
+    "revision": "5032822b89526db82251",
+    "url": "/static/js/2.04ba3136.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.04ba3136.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3dd52b0f1562c0de2e5c",
+    "url": "/static/js/main.7489479b.chunk.js"
+  },
+  {
+    "revision": "1192dd456684503318ee",
+    "url": "/static/js/runtime-main.b3798cb8.js"
+  }
+]);
