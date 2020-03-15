92b2abbb6921f0493224e78eb4f867be1d732f38
Bernard Potocki <bernard.potocki@imanel.org>
2020-03-15 23:29:32 +0100

    Move all css to single file

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -3,8 +3,6 @@ import React from 'react';
 import DownloadLink from 'components/DownloadLink'
 import FieldList from 'components/FieldList'
 
-import './App.css';
-
 const App = () => (
   <React.Fragment>
     <DownloadLink />
